ce55329103fb447f85843bfda2c1312dbfd73533
tanujbhardwaj <tanuj.bhardwaj@1mg.com>
2023-07-31 16:05:16 +0530

    new boiler

--- db/migrate/20230728184733_add_title_body_to_post.rb
+++
@@ -1,7 +0,0 @@
-class AddTitleBodyToPost < ActiveRecord::Migration[7.0]
-  def change
-    add_column :posts, :title, :string
-    add_column :posts, :body, :text
-    remove_column :posts, :user
-  end
-end
